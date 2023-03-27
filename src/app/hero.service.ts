4ee4866c8497b25984e0c519728a43dcdffbdcfa
Theocrsb <theo_crosbie@yahoo.fr>
2023-03-27 16:53:16 +0200

    heroservice en cours : 'https://angular.io/tutorial/tour-of-heroes/toh-pt4'

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './interface.hero';
 import { heroes } from './heroes';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HeroService {
   constructor() {}
-  getHeroes(): Hero[] {
-    return heroes;
+  getHeroes(): Observable<Hero[]> {
+    const heroesAll = of(heroes);
+    return heroesAll;
   }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { heroes } from '../heroes';
 import { Hero } from '../interface.hero';
+import { HeroService } from '../hero.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent {
+  constructor(private heroService: HeroService) {}
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
+  }
+
   heroes = [...heroes];
   selectedHero?: Hero;
   // on attribu le hero select pour le l'afficher
