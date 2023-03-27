c9173a8effb4c1f82893f28c7b7f5ee7506ef7cd
Theocrsb <theo_crosbie@yahoo.fr>
2023-03-27 10:55:06 +0200

    generate all view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { DetailsComponent } from './details/details.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    DashboardComponent,
+    DetailsComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule
